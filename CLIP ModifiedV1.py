2a2433fba53bf9a376e579cd8a460b0b97b560c2
Cornelio <cornelioabdimash@gmail.com>
2025-05-14 20:10:27 +0700

    Update CLIP ModifiedV1.py

--- CLIP ModifiedV1.py
+++ CLIP ModifiedV1.py
@@ -10,8 +10,8 @@ processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
 # Settings
 prompt = "young, friendly, and energetic south east asian college girl"
-image_folder = "/content/drive/MyDrive/MindyClip/Image/Enhanced"
-avoid_keywords = ["Dzine", "Ilustration"]
+image_folder = "./Dataset/Train\2_Enhance"
+avoid_keywords = ["bad_anatomy,bad_hands,three_hands,three_legs,bad_arms,missing_legs,missing_arms,poorly_drawn_face,poorly_rendered_hands,bad_face,fused_face,cloned_face,worst_face,three_crus,extra_crus,fused_crus,worst_feet,three_feet,fused_feet,fused_thigh,three_thigh,extra_thigh,worst_thigh,missing_fingers,extra_fingers,ugly_fingers,long_fingers,bad_composition,horn,extra_eyes,huge_eyes,2girl,amputation,disconnected_limbs,cartoon,cg,3d,unreal,animate,cgi,render,artwork,illustration,3d_render,cinema_4d,artstation,octane_render,mutated_body_parts,painting,oil_painting,2d,sketch,bad_photography,bad_photo,deviant_art,aberrations,abstract,anime,black_and_white,collapsed,conjoined,creative,drawing,extra_windows,harsh_lighting,jpeg_artifacts,low_saturation,monochrome,multiple_levels,overexposed,oversaturated,photoshop,rotten,surreal,twisted,UI,underexposed,unnatural,unreal_engine,unrealistic,video_game,deformed_body_features"]
 
 # Iterate over image versions (assuming images named 5.jpg ... 15.jpg or 5.png, etc.)
 results = []
